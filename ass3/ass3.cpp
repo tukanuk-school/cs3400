a0bc1fdf1f56cc5741f69abd5fec3e2d959f5779
Tukanuk <benjamin@idavidson.ca>
2019-02-25 23:41:12 -0500

    updates

--- ass3/ass3.cpp
+++ ass3/ass3.cpp
@@ -5,8 +5,15 @@
 #include <cstring>
 #include <optional>
 #include <iostream>
-// #include <boost/filesystem.hpp>
-#include <filesystem>
+#include <boost/filesystem.hpp>
+// #include <filesystem>
+
+using all_inputs_type = std::vector<std::shared_ptr<program_input> >;
+
+std::vector<bool> read_all_inputs(all_inputs_type& /*ai*/)
+{
+    return {}; // return a default constructed std::vector<bool>
+}
 
 
 class program_input
@@ -24,8 +31,8 @@ std::ostream& output_usage(std::ostream& os, int argc, char *argv[])
 
 int main (int argc, char *argv[]) 
 {
-    // namespace fs = boost::filesystem; // using boost library on macos rather than adjust complier version
-    namespace fs = std::filesystem;
+    namespace fs = boost::filesystem; // using boost library on macos rather than adjust complier version
+    // namespace fs = std::filesystem;
     using namespace std;
 
     if (argc == 1)
@@ -34,14 +41,35 @@ int main (int argc, char *argv[])
         return 1;
     }
 
+    optional<string> scan_directory;
 
+    for (int args_pos = 1 ; args_pos < argc ; ++args_pos)
+    {
+        if (!strcmp(argv[args_pos], "-d") )
+        {
+            if (args_pos + 1 >= argc)
+            {
+                output_usage(cerr, argc, argv) << "\tNOTE: The -d option appreas without a <directory> argument.\n";
+                return 2;
+            }
 
-    using all_inputs_type = std::vector<std::shared_ptr<program_input> >;
-    std::cout << "test";
+            // assign argv to directory
+            ++args_pos;
+            scan_directory = argv[args_pos];
+        }
+        else
+        {
+            output_usage(cerr, argc, argv) << "\tNOTE: Invalid argrument(s) provided.\n";
+        }
+
+        all_inputs_type all_inputs; // hold all discovered program input files 
+
+        for (auto& entry : fs::recursive_directory_iterator(scan_directory.value()))
+        {
+            cout << "DEBUG: discovered: " << entry.path() << '\n';
+        }
+        read_all_inputs(all_inputs);
+    }
 }
 
-// std::vector<bool> read_all_inputs(all_inputs_type& /*ai*/)
-// {
-//     return {}; // return a default constructed std::vector<bool>
-// }
 
