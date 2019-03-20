c109896bdddba7775a4010453679b45df0499d82
Ben Davidson <benjamin@idavidson.ca>
2019-03-19 20:07:07 -0400

    update

--- lec7b.cxx
+++ lec7b.cxx
@@ -2,7 +2,42 @@
 
 using namespace std;
 
+struct A 
+{ 
+	A()
+	{
+		cout << "A::A() " << this << "\n"
+	}
+	~A()
+	{
+		cout << "A::A() " << this << "\n"
+	}
 
+	A(A const& a)
+	{
+		cout << "A::A(COPY: " << &a << ") " << this << "\n"
+	}
+
+	A& operator = (A const& a)
+	{
+		cout << "A& : " << &a << ") " << this << "\n"
+	}
+
+};
+
+struct B { };
+
+struct Foo
+{
+	A a;
+	B b;
+
+	Foo()
+	{
+		a = A();
+		b = B();
+	}
+};
 
 int main()
 {
